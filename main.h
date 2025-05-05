c6b7f7213b8e6e1a9f743b55dddec666b47fa5ce
ilgar31 <109583499+ilgar31@users.noreply.github.com>
2025-05-05 03:05:17 +0300

    Create main.h

---
+++ main.h
@@ -0,0 +1,2 @@
+double root(double (*f1)(double), double (*f2)(double), double a, double b, double eps, char print, char iter);
+double integral(double (*f)(double), double a, double b, double eps);
